32c6bfcc56c09153df348c7f3d213f034be6d173
suhyun <qkrtngus116@naver.com>
2023-05-17 08:40:41 +0900

    스프링 핵심 원리 이해2

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,23 +1,42 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-//기획자같은 역할을 하는 클래스
+//기획자같은 역할을 하는 클래스 및 AppConfig 리팩터링 후(역할에 따른 구현이 잘 보이게 해준다.)
+@Configuration
+//@Configuration는 구성정보를 담당해준다는 것의 의미임.
 public class AppConfig {
 
+    @Bean
     //생성자 주입
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-
+    @Bean
+    public DiscountPolicy discountPolicy(){
+        //return new FixDiscountPolicy();
+        //정률할인정책 적용
+        return new RateDiscountPolicy();
+    }
 }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,22 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
         //MemberService memberService = new MemberServiceImpl();
         //1L이라는 회원아이디, memberA라는 회원이름, VIP라는 등급
+
+        //ApplicationContext는 내가 등록한 Bean들을 모두 관리해주는 객체임. + 스프링컨테이너라고 함.
+        //AppConfig에 있는 환경설정정보를 가지고 스프링 컨테이너에 모두 등록하고 관리해줌.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //AppConfig에 memberService라는 메서드이름으로 등록된 것을 찾을 것이고, 이에 대한 반환타입은 MemberService야. 라고 알려줌.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
